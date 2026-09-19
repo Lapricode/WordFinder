d586178ce7e2266c82acc54d26c8c6eca36cddd6
Lapricode <prilampros@gmail.com>
2026-09-19 15:12:02 +0300

    small changes

--- wf_modals.py
+++ wf_modals.py
@@ -280,7 +280,7 @@ class InfoModal:
             "bullet",
         ),
         (
-            f"]/[ increase/decrease words per raw in the results panel.",
+            f"]/[ {special_chars["-"]} increase/decrease words per raw in the results panel.",
             "bullet",
         ),
         (
